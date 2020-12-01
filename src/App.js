da6ec1b53004af64d08721627a816cf2e0d2104d
mwmachingura <mutsamaching@gmail.com>
2020-12-01 21:31:53 +0200

    Footer added to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+//External Library Imports
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
+
+//Local imports
+import Footer from './components/Footer'
 
 
 class App extends React.Component {
@@ -41,13 +45,17 @@ class App extends React.Component {
                         <Navbar.Brand>Mutsa Machingura</Navbar.Brand>
                         <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
                         <Navbar.Collapse id="navbar-toggle">
-                        <Nav className="ml-auto">
-                            <Link className="nav-link" to="/">Home</Link>
-                            <Link className="nav-link" to="/about">About</Link>
-                            <Link className="nav-link" to="/contact">Contact</Link>
-                        </Nav>
-            </Navbar.Collapse>
+                            <Nav className="ml-auto">
+                                <Link className="nav-link" to="/">Home</Link>
+                                <Link className="nav-link" to="/about">About</Link>
+                                <Link className="nav-link" to="/contact">Contact</Link>
+                            </Nav>
+                        </Navbar.Collapse>
                     </Navbar>
+
+
+                    <Footer />
+
                 </Container>
             </Router>
         ];
