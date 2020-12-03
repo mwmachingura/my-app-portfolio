506d4942911e21d28e37e015cf66019a675f154a
mwmachingura <mutsamaching@gmail.com>
2020-12-03 11:06:25 +0200

    Contact Page from created and ready for api insert.

--- src/pages/Contactpage.js
+++ src/pages/Contactpage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import Table from '../components/Table';
 import Hero from '../components/Hero';
+
 
 function Contactpage(props) {
     return (
         <div>
             <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
+            <Table />
         </div>
     );
 }
